a9bdb922e06a811fb723f5974f3c744d1c9334cf
dev-saiful <saifulcseian@gmail.com>
2024-05-25 23:42:42 +0600

    routes update

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,10 +1,14 @@
 import {Router} from "express";
 import {admin,
     customer,
+    deleteUser,
+    getUserById,
+    getUsers,
     login,
     logout,
     refreshAccessToken,
-    register} from "../controllers/user.controller.js";
+    register,
+    updateUser} from "../controllers/user.controller.js";
 import { 
     auth, 
     isAdmin, 
@@ -14,7 +18,7 @@ const userRoute = Router();
 /**
  * @desc Login User
  * @method POST
- * @route http://localhost:{PORT}/api/v1/user/login
+ * @route http://localhost:{PORT}/api/v1/users/login
  * @params name,email,password,role
  * @access public
  */
@@ -22,39 +26,80 @@ userRoute.post("/login",login);
 /**
  * @desc Register User
  * @method POST
- * @route http://localhost:{PORT}/api/v1/user/register
+ * @route http://localhost:{PORT}/api/v1/users/register
  * @params name,email,password,role
  * @access public
  */
 userRoute.post("/register",register);
 /**
- * @desc Admin Profile
- * @method GET
- * @route http://localhost:{PORT}/api/v1/user/admin
- * @access private
- */
-userRoute.get("/admin",auth,isAdmin,admin);
-/**
- * @desc User Profile
- * @method GET
- * @route http://localhost:{PORT}/api/v1/user/customer
- * @access private
- */
-userRoute.get("/customer",auth,isUser,customer);
-/**
  * @desc User Logout
  * @method POST
- * @route http://localhost:{PORT}/api/v1/user/logout
+ * @route http://localhost:{PORT}/api/v1/users/logout
  * @access private
  */
 userRoute.post("/logout",auth,logout);
 /**
  * @desc Token Refresh
  * @method POST
- * @route http://localhost:{PORT}/api/v1/user/refresh-token
+ * @route http://localhost:{PORT}/api/v1/users/refresh-token
  * @access private
  */
 userRoute.post("/refresh-token",refreshAccessToken);
+
+
+
+/**
+ * @desc Admin Profile
+ * @method GET
+ * @route http://localhost:{PORT}/api/v1/users/admin
+ * @access private
+ */
+userRoute.get("/admin",auth,isAdmin,admin);
+/**
+ * @desc All Users 
+ * @method GET
+ * @route http://localhost:{PORT}/api/v1/users
+ * @access private
+ */
+userRoute.get("/",auth,isAdmin,getUsers);
+/**
+ * @desc Individual User
+ * @method GET
+ * @route http://localhost:{PORT}/api/v1/users
+ * @access private
+ */
+userRoute.get("/:id",auth,isAdmin,getUserById);
+/**
+ * @desc Update Individual User
+ * @method PUT
+ * @route http://localhost:{PORT}/api/v1/users
+ * @access private
+ */
+userRoute.put("/:id",auth,isAdmin,updateUser);
+/**
+ * @desc DELETE Individual User
+ * @method DELETE
+ * @route http://localhost:{PORT}/api/v1/users
+ * @access private
+ */
+userRoute.delete("/:id",auth,isAdmin,deleteUser);
+
+
+/**
+ * @desc User Profile
+ * @method GET
+ * @route http://localhost:{PORT}/api/v1/users/customer
+ * @access private
+ */
+userRoute.get("/customer",auth,isUser,customer);
+/**
+ * @desc Update User Profile
+ * @method PUT
+ * @route http://localhost:{PORT}/api/v1/users/:id
+ * @access private
+ */
+userRoute.put("/:id",auth,isUser,updateUser);
+
 
 
 export default userRoute;
