3fe82dd75832cbc6b77efd4a00849183d4f47c0a
dev-saiful <saifulcseian@gmail.com>
2024-08-31 21:42:55 +0600

    cloudinary include

--- server/app.js
+++ server/app.js
@@ -30,33 +30,10 @@ app.get("/api/config/paypal",(req,res)=>{
     res.send({clientId : process.env.PAYPAL_CLIENT_ID});
 });
 
-    // const __dirname = path.resolve();
-    // const __dirname = path.join(path.resolve(),"..");
-
-if(process.env.NODE_ENV==="production")
-{
-        const __dirname = path.resolve();
-    // console.log(path.join(__dirname,"/uploads"));
-    console.log(__dirname);
-    // set static folder
-    app.use("/uploads",express.static("/var/data/uploads"));
-    app.use(express.static(path.join(__dirname,"..","/client/dist")));
-
-    // any route that is not api will be redirected to index.html
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"..","client","dist","index.html"));
-    });
-}
-else
-{
-const __dirname = path.resolve();
-app.use("/uploads",express.static(path.join(__dirname,"..","/uploads")));
-console.log(path.join(__dirname));
-    app.get("/",(req,res)=>{
+app.get("/",(req,res)=>{
       
         res.send("Api is running");
-    });
-}
+});
 
 app.use(notFound);
 app.use(errorHandler);
--- server/routes/upload.routes.js
+++ server/routes/upload.routes.js
@@ -3,13 +3,14 @@ import { Router } from "express";
 import multer from "multer";
 import {ApiError} from "../utils/apiError.js";
 import {ApiResponse} from "../utils/apiResponse.js";
+import { imageUpload } from "../utils/cloudinary.js";
 
 const uploadRoute = Router();
 
 const storage = multer.diskStorage({
     destination(req,file,cb)
     {
-        cb(null,"../uploads"); // null is for error
+        cb(null,"uploads"); // null is for error
     },
     filename(req,file,cb)
     {
@@ -36,15 +37,21 @@ const upload = multer({ storage, fileFilter });
 const uploadSingleImage = upload.single('image');
 
 uploadRoute.post("/", (req, res) => {
-  uploadSingleImage(req, res, function (err) {
+  uploadSingleImage(req, res, async function (err) {
       if (err) {
           return res.status(400).json(new ApiError(400, err.message));
       }
       if (!req.file) {
           return res.status(400).json(new ApiError(400, "No file uploaded"));
       }
+    //   uploading in cloudinary
+    const {secure_url} = await imageUpload(req.file.path,"products");
+    if(!secure_url)
+    {
+        return res.status(400).json(new ApiError(400,"Image upload failed to cloudinary"));
+    }
       res.status(200).json(
-          new ApiResponse(200, `${req.file.path}` , "Image uploaded successfully")
+          new ApiResponse(200, `${secure_url}` , "Image uploaded successfully")
       );
   });
 });
